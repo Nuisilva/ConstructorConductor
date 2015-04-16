0b93583f4dddc658b6f8e3c8e37a06facc73a1b5
Nuisilva <sigbin2000@gmail.com>
2015-04-15 19:55:05 -0600

    Update instanceArray.js

--- instanceArray.js
+++ instanceArray.js
@@ -13,29 +13,42 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
-
+var users = [];
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
-  //code here
+var Tyler = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+  users.push(Tyler);
+
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+console.log('Tyler\'s information is ' , Tyler  )
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
-  //code here
+  var Lenny = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+  users.push(Lenny);
+  
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+  var Nui = new User('Nui', "sigbin2000@gmail.com", 'openit');
+  users.push(Nui);
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
-
+var sayName =  function (users){
+  console.log('All my users names are '){
+    for (var i=0; i < users.length; i++){
+      console.log(users[i].name);
+    }
+  }
+}
+for (var i=0; i < users.length; i++){
+  console.log('All my users names are ', users[i].name);
+};
   //code here
